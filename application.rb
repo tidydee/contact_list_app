99491806492ed64cc03a1ee85423819d1d15de1a
dawidspamer <dawidspamer@gmail.com>
2014-03-17 16:24:26 +0000

    Adding 'list' 'show' 'find' 'quit' 'CSV' functionality

--- application.rb
+++ application.rb
@@ -3,35 +3,145 @@ require 'colorize'
 class Application
 
   def run
-    show_main_menu
-    input = gets.chomp
+    should_run = true
+    while should_run do
+      show_main_menu
+      input = gets.chomp.downcase
       case input
       when "quit"
         quit
-        
-        
-      
-  
-
-        
+        should_run = false
+      when "new"
+        new_contact 
+      when "list"
+        list            
+      when "show"
+        show
+      when "find"
+        find
       end
-
-
+    end
   end
 
   private
 
   # Prints the main menu only
   def show_main_menu
-    puts "Welcome to the best contact list managing application. What's would you like to do next?".colorize( :blue )
-    puts " new      - Create a new contact".colorize( :green )
-    puts " list     - List all contacts".colorize( :green )
-    puts " quit     - Exit Application".colorize( :green )
+    puts "Welcome to the best contact list managing application.".upcase.colorize( :blue )
+    puts "What would you like to do next?".upcase.colorize( :green )
+    puts " new      - Create a new contact.".colorize( :green )
+    puts " list     - List all contacts.".colorize( :green )
+    puts " show     - Displays contact's ID and contact details.".colorize( :green )
+    puts " find     - Displays contact's name and contact details by name.".colorize( :green )
+    puts " quit     - Exit Application.".colorize( :green )
     print ">> "
   end
 
   def quit
-    abort("Good-Bye!!")
+    puts "Good-Bye!!".colorize( :blue )
+  end
+
+  def find
+    puts "Please type the contact's first name you would like to find."
+    contact_name = gets.chomp
+
+    # found_contact = []
+    # Contact.all.each do |contact|
+    #   if contact.fname == contact_name
+    #    found_contact << contact
+    #   end 
+    # end
+
+    found_contact = Contact.all.find_all {|contact| contact.fname == contact_name }
+
+    if !found_contact.nil?
+      puts found_contact
+    else
+      puts "Could not find contact with name #{contact_name}".colorize( :red )
+    end
+  end
+
+
+  def show
+    puts "Please type contact's ID."
+    id = gets.chomp.to_i
+      
+    found_contact = nil
+    Contact.all.each do |contact|
+      if id == contact.id
+       found_contact = contact
+      end 
+    end
+
+    if !found_contact.nil?
+      puts found_contact
+    else
+      puts "Could not find contact with id #{id}".colorize( :red )
+    end
+  end
+
+   def list
+    Contact.all.each do |c_list| 
+      puts c_list
+    end
+  end
+  
+  def new_contact
+    
+    puts "Please type contact's email address."
+    email = gets.chomp
+    email_check = Contact.all.find {|contact| contact.email == email}
+
+      if !email_check.nil?
+        puts ("A contact with this email already exists!").upcase.colorize( :red ) 
+        puts ("Duplicated email address are not allowed.").upcase.colorize( :red )
+      else
+        puts "Please type contact's first name."
+        fname = gets.chomp
+        puts "Please type contact's last name."
+        lname = gets.chomp
+        numbers = add_phones
+
+        contact = Contact.create(fname, lname, email, numbers)
+        contactString = "#{contact}".colorize(:green)
+        puts "New contact #{contactString} has been added."
+      end
+  end
+
+  def add_phones
+    phones = Hash.new
+    if true
+      puts "Please describe the phone number you'd like to add. Ex: 'mobile.'"
+      user_phone_description = gets.chomp
+      puts "Please enter the phone number for #{user_phone_description}:"
+      phones[user_phone_description] = gets.chomp
+      puts "Number added. To add another one, type 'y', to return the main screen, type 'n'."
+      user_add_another_number = gets.chomp
+        if user_add_another_number == "n"
+          return phones
+        end
+    end
   end
 
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- contact.rb
+++ contact.rb
@@ -1,33 +1,52 @@
 class Contact
 
   ## In-memory list of contacts
+
+  attr_accessor :fname, :lname, :email, :id, :list, :numbers
   @@contacts = []
 
-  attr_accessor :name
-  attr_accessor :email
-
-  def initialize(name, email)
-    # TODO: assign local variables to instance variables
+  def initialize(fname, lname, email, numbers)
+   @fname = fname
+   @lname = lname
+   @email = email
+   @numbers = numbers
+   @id = @@contacts.length + 1
   end
 
   def to_s
     # TODO: return string representation of Contact
+    "#{@id}: #{@fname} #{@lname} (#{@email}) #{@numbers}"  
   end
+
 
   ## Class Methods
   class << self
-    def create(name, email)
-      # TODO: Will initialize a contact as well as add it to the list of contacts
+
+    def create(fname, lname, email, numbers = {})
+      contact = Contact.new(fname, lname, email, numbers)
+      @@contacts << contact
+      return contact
     end
 
-    def find(index)
-      # TODO: Will find and return contact by index
+    def all_CSV
+      @@contacts      
+    end
+
+    def all_CSV=(csv_file)
+      @@contacts = csv_file
     end
 
     def all
-      # TODO: Return the list of contacts, as is
+      @@contacts
+    end
+
+    def all=(contacts)
+      @@contact = contacts
+    end
+
+    def destroy_all
+      @@contacts = []
     end
   end
 
 end
- 
